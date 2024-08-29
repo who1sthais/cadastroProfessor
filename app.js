f9b7cf853e537bbb1a5758c24c00b7da4e453894
Thais Vitoria Oliveira Da Silva <07587278109@dccbra.ifb.local>
2024-08-29 16:10:20 -0300

    utilizando model view controller

--- app.js
+++ app.js
@@ -1,122 +1,46 @@
-const express = require('express');
-const mysql = require('mysql2');
-const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false})) 
-app.use(bodyParser.json()) 
-// Create connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'escola'
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+const alunosRouter = require('./routes/alunos'); 
+const professorRouter = require('./routes/professor');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',alunosRouter);
+app.use('/',professorRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// Connect
-db.connect((err) => {
-  if(err){
-      throw err;
-  }
-  console.log('MySql Connected...');
-});
+// error handler
+app.use((err, req, res, next) => {
+  // Configurar mensagem e erro para renderizar
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.get('/', (req, res) => {
-  res.send(`
-  <h1>Menu</h1>
-  <ul>
-    <li><a href="/getalunos">Listar Alunos</a></li>
-    <li><a href="/deletealuno">Apagar Aluno</a></li>
-    <li><a href="/addaluno">Apagar Aluno</a></li>
-  </ul> 
-  `);
-});
-
-// Insert aluno form
-app.get('/addaluno', (req, res) => {
-  res.send(`
-  <h1>Menu</h1>
-  <ul>
-    <li><a href="/getalunos">Listar Alunos</a></li>
-    <li><a href="/deletealuno">Apagar Aluno</a></li>
-  </ul>
-  <br>
-    <form action="/addaluno" method="post">
-      <label>Nome:</label>
-      <input type="text" name="nome" required><br><br>
-      <label>Idade:</label>
-      <input type="number" name="idade" required><br><br>
-      <input type="submit" value="Submit">
-    </form>
-  `);
-});
-
-
-app.get('/deletealuno', (req, res) => {
-    res.send(`
-    <h1>Menu</h1>
-    <ul>
-      <li><a href="/addaluno">Adicionar Aluno</a></li>
-      <li><a href="/getalunos">Listar Alunos</a></li>
-      </ul>
-    <br>
-      <form action="/deletealuno" method="post">
-        <label>Id aluno:</label>
-        <input type="text" name="id" required><br><br>
-       <br>
-        <input type="submit" value="Submit">
-      </form>
-    `);
-  });
-
-
-
-  app.post('/deletealuno', (req, res) => {
-    let query = db.query('DELETE FROM aluno where id=?',
-   [req.body.id],
-   (err, result) => {
-       if(err) throw err;
-       console.log(result);
-       res.redirect('/getalunos');
-   });
- });
-  
-
-// Insert aluno
-app.post('/addaluno', (req, res) => {
-   let query = db.query('INSERT INTO aluno (idade, nome) Values(?,?)',
-  [req.body.idade,req.body.nome],
-  (err, result) => {
-      if(err) throw err;
-      console.log(result);
-      res.redirect('/getalunos');
+  // Renderiza a página de erro
+  res.status(err.status || 500);
+  res.render('error', {
+    title: 'Erro',       // Aqui definimos o título da página de erro
+    message: err.message,
+    error: err
   });
 });
 
-// Select alunos
-app.get('/getalunos', (req, res) => {
-  let sql = 'SELECT * FROM aluno';
-  let query = db.query(sql, (err, results) => {
-      if(err) throw err;
-      console.log(results);
-      res.send(`
-      <h1>Menu</h1>
-    <ul>
-      <li><a href="/addaluno">Adicionar Aluno</a></li>
-      <li><a href="/deletealuno">Apagar Aluno</a></li>
-    </ul>
-    <br>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Idade</th>
-          </tr>
-          ${results.map(aluno => `<tr><td>${aluno.id}</td><td>${aluno.nome}</td><td>${aluno.idade}</td></tr>`).join('')}
-        </table>
-      `);
-  });
-});
-
-app.listen('3000', () => {
-    console.log('Server started on port 3000');
-});
+module.exports = app;
